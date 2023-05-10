082bec70c2ca62a9237fc95f1252470036f92f58
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-05-10 16:22:15 -0300

    feat: only numbers in cpf

--- src/schemas/customer.schema.js
+++ src/schemas/customer.schema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const customerSchema = Joi.object({
   name: Joi.string().trim().required(),
-  cpf: Joi.string().trim().required().length(11),
+  cpf: Joi.string().trim().replace(/\D/, "").required().length(11),
   phone: Joi.string().trim().required().min(10).max(11),
   birthday: Joi.string().isoDate().required(),
 });
